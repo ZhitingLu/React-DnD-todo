28367c7039e4655c3625eeaa14820172e914c5b1
Zhiting Lu <zlu@asbarcelona.com>
2023-06-30 13:18:21 +0200

    Created react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-amber-100 text-amber-600">
+        HI
+      </div>
+    </>
+  )
+}
+
+export default App
