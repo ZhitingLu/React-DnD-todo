f251d396604bf305dfec960cf3f17d397b7e2d28
ZhitingLu <zhitinglu@outlook.com>
2023-07-02 08:27:43 +0200

    added react hot toast pop up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CreateTasks from './components/CreateTasks';
 import ListTasks from './components/ListTasks';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  console.log("tasks:", tasks);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")));
+  }, [])
+
   return (
     <>
-      <div className="bg-slate-100 w-screen h-screen flex flex-col items-center justify-center">
+      <Toaster />
+      <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16">
         <CreateTasks tasks={tasks} setTasks={setTasks} />
         <ListTasks tasks={tasks} setTasks={setTasks} />
       </div>
