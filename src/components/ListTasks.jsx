da098366b16092cb2cd85f0e36740a05112690ee
ZhitingLu <zhitinglu@outlook.com>
2023-07-04 08:33:40 +0200

    added Task component

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -7,9 +7,9 @@ const ListTasks = ({ tasks, setTasks }) => {
   const [closed, setClosed] = useState([]);
 
   useEffect(() => {
-    const fTodos = tasks.filter(task => task.status === "todo");
-    const fInProgress = tasks.filter(task => task.status === "inprogress");
-    const fClosed = tasks.filter(task => task.status === "closed");
+    const fTodos = tasks.filter((task) => task.status === "todo");
+    const fInProgress = tasks.filter((task) => task.status === "inprogress");
+    const fClosed = tasks.filter((task) => task.status === "closed");
 
     setTodos(fTodos);
     setInProgress(fInProgress);
@@ -48,7 +48,7 @@ const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
     bg = "bg-purple-500";
     tasksToMap = inProgress;
   }
-  
+
   if (status === "closed") {
     text = "Closed";
     bg = "bg-green-500";
@@ -58,7 +58,11 @@ const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
   return (
     <>
       <div className={`w-64 `}>
-        <Header text={text} bg={bg} count={tasksToMap.length} /> List
+        <Header text={text} bg={bg} count={tasksToMap.length} />
+        {tasksToMap.length > 0 &&
+          tasksToMap.map((task) => (
+            <Task key={task.id} tasks={tasks} setTasks={setTasks} task={task} />
+          ))}
       </div>
     </>
   );
@@ -74,6 +78,39 @@ const Header = ({ text, bg, count }) => {
         <div className="ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
           {count}
         </div>
+      </div>
+    </>
+  );
+};
+
+const Task = ({ task, tasks, setTasks }) => {
+
+    const handleRemove = (id) => {
+        console.log(id)
+
+        const fTasks = tasks.filter(task => task.id !== id)
+    }
+
+  return (
+    <>
+      <div className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab`}>
+        <p>{task.name}</p>
+        <button className="absolute bottom-1 right-1 text-slate-400" onClick={() => handleRemove(task.id)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
       </div>
     </>
   );
